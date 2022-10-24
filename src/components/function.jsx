123ae31ef4b8c41038b665dba08fc1ad6638a23a
Ihor <ihorrykun@gmail.com>
2022-10-24 21:03:46 +0300

    1

--- src/components/function.jsx
+++
@@ -1,5 +0,0 @@
-export default adp;
-
-function adp() {
-  return `<p>hello worl</p>`;
-}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Children } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Add from './components/function';
+import user from '../src/user';
 
+const elem = user;
+console.log(elem);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Add />);
+root.render();
