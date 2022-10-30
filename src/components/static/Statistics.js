9410384fe60297ba37097839ff26f7c12331e3db
Ihor <ihorrykun@gmail.com>
2022-10-30 12:09:46 +0200

    update

--- src/components/static/Statistics.js
+++ src/components/static/Statistics.js
@@ -3,17 +3,19 @@ import css from '../static/statistic.module.css';
 export const StaticId = ({ title, stats }) => {
   return (
     <section className={css.section}>
-      <h2 className={css.title}>{title}</h2>
-      <ul className={css.list}>
-        {stats.map(({ label, percentage, id }) => {
-          return (
-            <li className={css.list__item} key={id}>
-              <span>{label}</span>
-              <span className={css.percentage}>{percentage}%</span>
-            </li>
-          );
-        })}
-      </ul>
+      <div className={css.container}>
+        <h2 className={css.title}>{title}</h2>
+        <ul className={css.list}>
+          {stats.map(({ label, percentage, id }) => {
+            return (
+              <li className={css.list__item} key={id}>
+                <span>{label}</span>
+                <span className={css.percentage}>{percentage}%</span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </section>
   );
 };
